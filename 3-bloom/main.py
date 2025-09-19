53f9103f0ad90b4531d6766af5927af17f1b22e6
Joao Klug <joaoklug18@gmail.com>
2025-09-19 16:25:09 -0300

    Passo 1

--- 3-bloom/main.py
+++ 3-bloom/main.py
@@ -5,11 +5,12 @@ import numpy as np
 import cv2
 INPUT_IMAGE_1 = "assets/GT2.BMP"
 INPUT_IMAGE_2 = "assets/Wind Waker GC.bmp"
-LUMINOSO = 170
+LUMINOSO = 0.55
+PRETO = 0
 #===============================================================================
 def main ():
 
-    img = cv2.imread (INPUT_IMAGE_1)
+    img = cv2.imread (INPUT_IMAGE_2).astype(np.float32) / 255.0
     
     if img is None:
         print ('Erro abrindo a imagem.\n')
@@ -18,13 +19,20 @@ def main ():
     altura = img.shape[0]
     largura = img.shape[1]
     
-    brigth_pass = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    brigth_pass[:,:,2] = np.where(brigth_pass[:,:,2] < LUMINOSO, 0, brigth_pass[:,:,2])
+    img_grey = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY).astype(np.float32)
 
-    img_out = cv2.cvtColor(brigth_pass, cv2.COLOR_HSV2BGR)
+    for i in range (altura):
+        for j in range (largura):
+            if(img_grey[i][j] < LUMINOSO):
+                img[i][j][0] = PRETO
+                img[i][j][1] = PRETO
+                img[i][j][2] = PRETO
 
-    cv2.imshow ('03 - out', img_out)
-    cv2.imwrite ('out/03 - out.png', img_out)
+
+    
+    cv2.imshow ('03 - out', (img*255.0).astype(np.uint8))
+    cv2.waitKey (0)
+    cv2.imwrite ('out/03 - out.png', (img*255.0).astype(np.uint8))
     cv2.waitKey ()
     cv2.destroyAllWindows ()
 
