f1d36444f411e38583db1fe2523bd19bcf04edf5
Joao Klug <joaoklug18@gmail.com>
2025-10-07 14:17:24 -0300

    binariza final

--- 4-arroz/main.py
+++ 4-arroz/main.py
@@ -15,30 +15,29 @@ import cv2
 INPUT_IMAGE = 'assets/60.bmp'
 ALTURA_MIN = 5
 LARGURA_MIN = 5
-THRESHOLD = 0.6 #Talvez otimizar atraves de histograma (na media funciona mas pode quebrar)
-JANELA = 55 #Otimizar de acordo com o tamanho da imagem
-CONT_BLURS = 20
+JANELA_PERCENT = 0.15 #porcentagem do tamanho da imagem
 ARROZ = 1
 FUNDO = 0
 #===============================================================================
 
-def binariza (img, threshold):
+def binariza (img):
 
-    buffer = img.copy()
-    for cont in range(CONT_BLURS):
-        buffer = cv2.blur(buffer, (JANELA, JANELA))
-
+    janela = int(min(img.shape[0], img.shape[1])*JANELA_PERCENT)
+    if janela % 2 == 0:
+        janela += 1
+    
+    buffer = cv2.GaussianBlur(img, (janela, janela),0)
 
     buffer = img - buffer
-    buffer = cv2.normalize(buffer, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-
-    #Otimizar treshold com o buffer nesse momento
-    cv2.imwrite ('out/01 - diff.png', (buffer*255).astype(np.uint8))
-
-    buffer = np.where(buffer > threshold, ARROZ, FUNDO).astype(np.float32)
     
-    kernel = np.ones((3,3),np.float32)
+    buffer = cv2.normalize(buffer, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    
+    img_binarizada = cv2.threshold((buffer * 255).astype(np.uint8), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+   
+    buffer = (img_binarizada / 255.0).astype(np.float32)
 
+    kernel = np.ones((3,3),np.float32)
+    
     buffer = cv2.erode(buffer, kernel, iterations = 1)
     buffer = cv2.dilate(buffer, kernel, iterations = 1)
 
@@ -121,14 +120,12 @@ def main ():
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
 
-    # É uma boa prática manter o shape com 3 valores, independente da imagem ser
-    # colorida ou não. Também já convertemos para float32.
     img = img.astype (np.float32) / 255.0
 
     # Mantém uma cópia colorida para desenhar a saída.
     img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
 
-    img = binariza (img, THRESHOLD)
+    img = binariza (img)
     cv2.imshow ('01 - binarizada', img)
     cv2.imwrite ('out/01 - binarizada.png', (img*255).astype(np.uint8))
 
